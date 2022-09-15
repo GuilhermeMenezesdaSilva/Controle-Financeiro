cf4544f055bf0fee4eb10c82073ad01e370b6f53
brunoecp <rm95315@fiap.com.br>
2022-09-15 11:41:10 -0300

    commitei e fdc

--- controlefinanceiropessoal/src/main/java/br/com/fiap/controlefinanceiro/PrimaryController.java
+++ controlefinanceiropessoal/src/main/java/br/com/fiap/controlefinanceiro/PrimaryController.java
@@ -1,36 +1,84 @@
 package br.com.fiap.controlefinanceiro;
 
-import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
-public class PrimaryController {
+
+public class PrimaryController implements Initializable{
     @FXML private TextField textFieldNomeConta;
     @FXML private TextField textFieldValorConta;
     @FXML private TextField textFieldDataDeVencimento;
-    @FXML private TextField textFieldCategoriaConta;
+    @FXML private ChoiceBox<String> choiceBoxCategoriaConta;
+    @FXML private ListView<Financas> listViewFinancas;  
 
-    @FXML private ListView<Financas> listView;
     private List<Financas> Lista = new ArrayList<>();
+  
+    String[] listaCategorias =  {"Salário de funcionários","Contas de água e de luz","Empréstimos"};
     
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        choiceBoxCategoriaConta.getItems().addAll(listaCategorias);
+    }
+
     public void cadastrar() {
-        String  nome            = textFieldNomeConta.getText();
-        double  valor           = Double.valueOf(textFieldValorConta.getText());
-        int     data            = Integer.valueOf(textFieldDataDeVencimento.getText());
-        String  categoria       = textFieldCategoriaConta.getText();
-        
+        var contas = carregarInfos();
+        Lista.add(contas);
+        System.out.println(Lista);
+        limparFormulario();
+       
+        listViewFinancas.getItems().add(contas);
+        atualizarLista();
+    }
+
+    public Financas carregarInfos() {
+        String  nome = textFieldNomeConta.getText();
+        double  valor = Double.valueOf(textFieldValorConta.getText());
+        int     data   = Integer.valueOf(textFieldDataDeVencimento.getText());
+        String categoria =choiceBoxCategoriaConta.getValue();
+        Financas financas = new Financas(nome, valor, data, categoria, false);
+        return financas;
+    }
+
+    public void erro(String mensagem) {
+        Alert alert = new Alert(AlertType.ERROR);
+        alert.setContentText(mensagem);
+        alert.show();
+    }
+
+    public void ordenarCategoria() {
+       // Lista.sort((o1,o2) -> String.(o1.getCategoriaConta(), o2.getCategoriaConta()));
+        atualizarLista();
+    }
+
+    public void ordenarData() { 
+        Lista.sort((o1,o2) -> Double.compare(o1.getDataPagamentoConta(), o2.getDataPagamentoConta()));
+        atualizarLista();
+    }
+
+    public void ordenarValor() {
+        Lista.sort((o1,o2) -> Double.compare(o1.getValorConta(), o2.getValorConta()));
+        atualizarLista();
+    }
+
+    private void atualizarLista(){
+        listViewFinancas.getItems().clear();
+        listViewFinancas.getItems().addAll(Lista);
+    }
+
+    private void limparFormulario(){
         textFieldNomeConta.setText("");
         textFieldValorConta.setText("");
         textFieldDataDeVencimento.setText("");
-        textFieldCategoriaConta.setText("");
-
-        Financas financas = new Financas(nome, valor, data, categoria, false);
-        Lista.add(financas);
-        System.out.println(financas);
-       
     }
+
 }
